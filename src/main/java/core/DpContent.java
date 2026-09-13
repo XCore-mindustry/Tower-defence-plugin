d08185c36eb4238e5cf251e561f29054ee365374
DronazU <curakovalexandar62@gmail.com>
2026-09-13 19:11:01 +0300

    If u deleat there comes indestrictible wall out - fixed

--- src/main/java/core/DpContent.java
+++ src/main/java/core/DpContent.java
@@ -1,9 +1,11 @@
 package core;
 
 import mindustry.Vars;
+import mindustry.content.Blocks;
 import mindustry.ctype.ContentType;
 import mindustry.game.EventType;
 import mindustry.world.Block;
+import mindustry.world.Tile;
 
 public class DpContent {
     public static Block
@@ -16,6 +18,19 @@ public class DpContent {
 
     public static void onBlockBuildBeginEvent(EventType.BlockBuildBeginEvent event) {
         if (!event.breaking) return;
+        if (event.tile.block() != dpRoadWall) return;
         event.tile.setNet(dpRoadWall, event.team, 0);
+    }
+
+    public static void onPickupEvent(EventType.PickupEvent event) {
+        if (event.build == null) return;
+
+        if (event.build.block == dpRoadWall) {
+            Tile tile = event.build.tile;
+
+            tile.setNet(
+                    Blocks.copperWall, event.build.team, event.build.rotation
+            );
+        }
     }
 }
